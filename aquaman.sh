1c340aa25e2d496831bf5d0ab921c9c607fa3b7f
Coll147 <aaroncoll147@gmail.com>
2026-02-18 17:16:00 +0100

    update the script i home it doesnt kaboom my tv stick

--- aquaman.sh
+++ aquaman.sh
@@ -20,19 +20,63 @@ fi
 
 # Packages to remove/disable
 PACKAGES=(
-    "com.ejemplo.app1"
-    "com.ejemplo.app2"
-    "com.google.android.youtube.tv"
+    # -- Google packages
+    "com.google.android.onetimeinitializer"
+    "com.android.onetimeinitializer"
+    "com.google.android.partnersetup"
+    "com.google.android.tts"
+    "com.google.android.feedback"
+    "com.google.android.backuptransport"
+    "com.google.android.syncadapters.contacts"
+    "com.google.android.syncadapters.calendar"
+    # -- Android
+    "com.google.android.tvlauncher"
+    "com.google.android.tv"
+    "com.google.android.tvrecommendations"
+    "com.google.android.tv.bugreportsender"
+    "com.google.android.marvin.talkback"
+    "com.android.htmlviewer"
+    "com.android.printspooler"
+    "com.android.wallpaperbackup"
+    "com.android.backupconfirm"
+    "com.android.sharedstoragebackup"
+    "com.google.android.tungsten.setupwraith"
+    "com.android.settings.intelligence"
+    # -- Xiaomi Things
+    "com.xiaomi.mitv.updateservice"
+    "com.xiaomi.mitv.res"
+    "com.xiaomi.android.tvsetup.partnercustomizer"
+    "com.xiaomo.tv.milegal"
+    "com.miui.tv.analytics"
+    "com.mitv.dream"
+    "com.mitv.milinkservice"
+    "com.mitv.tvhome.michannel"
+    "com.mitv.tvhome.atv"
+    "com.mitv.download.service"
+    "com.mitv.videoplayer"
+    "mitv.service"
+    "com.xm.webcontent"
+    "tv.alphonso.alphonso_eula"
+    "android.autoinstalls.config.xioami.mibox3"
+    "android.autoinstalls.config.xiaomi.mibox3"
+
+    # -- Preinstaled APPS
+    "com.google.android.videos"
+    "com.google.android.play.games"
+    "com.google.android.youtube.tvmusic"
+    "com.amazon.amazonvideo.livingroom"
     "com.netflix.ninja"
 )
 
-echo "===== PRO ANDROID SCRIPT - Mi TV Stick ====="
+echo "=== PRO ANDROID SCRIPT - Mi TV Stick ==="
+
+echo ">>> Removing / Nuking / Whatever the pkg list"
 
 for pkg in "${PACKAGES[@]}"; do
     echo "----------------------------------------"
     echo "[INFO] Processing package: $pkg"
 
-    # Try uninstalling for user 0
+    # Try uninstalling
     if adb shell pm uninstall --user 0 "$pkg"; then
         echo "[OK] Successfully uninstalled: $pkg"
     else
@@ -47,9 +91,21 @@ for pkg in "${PACKAGES[@]}"; do
     fi
 done
 
-echo "===== PROCESS COMPLETED ====="
+echo ">>> Miscelaneus Adjustments"
 
-# You can add extra adb commands below:
-# adb install your_app.apk
-# adb shell pm enable com.example.app
-# etc.
+adb shell settings put global window_animation_scale 0
+adb shell settings put global transition_animation_scale 0
+adb shell settings put global animator_duration_scale 0
+adb shell settings put global background_process_limit 0
+adb shell setprop persist.logd.size 1M
+
+adb shell settings put global device_config_sync_disabled_for_tests persistent
+
+echo ">>> Installing new launcher"
+
+curl -o launcher.apk https://github.com/spocky/miproja1/releases/download/4.68/ProjectivyLauncher-4.68-c82-xda-release.apk
+adb install launcher.apk
+
+echo "=== Alllllllll Ready ==="
+echo ">>> Rebooting the little thing :)"
+adb reboot
